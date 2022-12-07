f82a0afdad8896ab3ac2f0fafcfb28f18f7a3c91
Amal Nazeem <amalnazeem@gmail.com>
2022-12-07 14:41:33 -0500

    Add gems and fix speed glitch

---
+++ helicopter/Assets/Resources/Scripts/GemSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // infinite gem spawning
+        StartCoroutine(SpawnGems());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private IEnumerator SpawnGems()
+    {
+        while(true)
+        {
+            Instantiate(prefab, new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+			yield return new WaitForSeconds(Random.Range(1, 5));
+        }
+    }
+}
